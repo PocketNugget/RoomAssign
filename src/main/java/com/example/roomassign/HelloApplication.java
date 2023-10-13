1da94c8b1a6c0790b40da8bb0d3d7eb75a94fb68
PocketNugget <goben.ca.pkn@hotmail.com>
2023-10-12 20:25:03 -0600

    Esta versión tiene interfaz pero no funciona, la lógica sigue detras pero corre

--- src/main/java/com/example/roomassign/HelloApplication.java
+++ src/main/java/com/example/roomassign/HelloApplication.java
@@ -11,10 +11,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setResizable(false);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        stage.requestFocus();
     }
 //Holaaaaaa
     public static void main(String[] args) {
--- src/main/java/com/example/roomassign/HelloController.java
+++ src/main/java/com/example/roomassign/HelloController.java
@@ -1,14 +1,33 @@
 package com.example.roomassign;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.*;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
+    private Label welcomeText;
+    private Button SearchOn;
+    @FXML
+    private RadioButton CompTrueOn;
+    @FXML
+    protected void CompTrueOn() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    public void CompTrueOn(ActionEvent actionEvent) {
+
+    }
+
+    public void SearchOn(ActionEvent actionEvent) {
+    }
+
+    public void ProyTrueOn(ActionEvent actionEvent) {
+    }
+
+    public void EstudiantesIn(ActionEvent actionEvent) {
     }
 }
