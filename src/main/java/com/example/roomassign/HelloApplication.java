af0dd5bb3e1d519afd604f06ea947eb1ff06b3ea
PocketNugget <goben.ca.pkn@hotmail.com>
2023-10-12 21:34:20 -0600

    Versión con scroll choicebox funcional

--- src/main/java/com/example/roomassign/HelloApplication.java
+++ src/main/java/com/example/roomassign/HelloApplication.java
@@ -11,12 +11,11 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 430);
         stage.setResizable(false);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-        stage.requestFocus();
     }
 //Holaaaaaa
     public static void main(String[] args) {
--- src/main/java/com/example/roomassign/HelloController.java
+++ src/main/java/com/example/roomassign/HelloController.java
@@ -1,33 +1,56 @@
 package com.example.roomassign;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
-
+import javafx.scene.input.MouseEvent;
 
 public class HelloController {
+    ObservableList<String> aulas = FXCollections.observableArrayList("AG", "HU", "BI", "CE", "CN", "CI", "SL", "CS", "PU", "IA", "IOS", "LB", "LA", "NE");
 
     @FXML
     private Label welcomeText;
+    @FXML
     private Button SearchOn;
     @FXML
     private RadioButton CompTrueOn;
     @FXML
+    private ChoiceBox<String> EdificiosL; // Añade el tipo de datos (String) entre los <>.
+
+    @FXML
     protected void CompTrueOn() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    public void initialize() {
+        EdificiosL.setItems(aulas);
+    }
+
+    @FXML
     public void CompTrueOn(ActionEvent actionEvent) {
-
     }
 
+    @FXML
     public void SearchOn(ActionEvent actionEvent) {
+        // Aquí puedes obtener la selección del usuario
+        String seleccion = EdificiosL.getValue();
+        if (seleccion != null) {
+            System.out.println("Seleccionaste: " + seleccion);
+        }
     }
 
+    @FXML
     public void ProyTrueOn(ActionEvent actionEvent) {
     }
 
+    @FXML
     public void EstudiantesIn(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void EdificiosLON(MouseEvent mouseEvent) {
     }
 }
